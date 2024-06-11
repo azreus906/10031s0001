387dbdb19cd84edef3d3d7dab2e2823d34479d11
yusuke.funabashi <kobarutogaeru@gmail.com>
2024-06-11 17:24:59 +0900

    Modify to add error handling

--- 実践プログラミングC言語/exercise_C_01.c
+++ 実践プログラミングC言語/exercise_C_01.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_STRING_VALUE 20
 #define LARGER_PS1 1
@@ -8,6 +9,17 @@
 
 int scmp(char *ps1, char *ps2) {
 
+    int len_ps1 = strlen(ps1); /* ps1の文字列の長さを取得 */
+    int len_ps2 = strlen(ps2); /* ps2の文字列の長さを取得 */
+
+    /* ps1またはps2が不正な値だった場合はERROR_VALUEでreturnする*/
+    if(ps1 == NULL || ps2 == NULL) {
+        return ERROR_VALUE;
+    } else if(len_ps1 <= 0 || len_ps2 <= 0 || len_ps1 > MAX_STRING_VALUE || len_ps2 > MAX_STRING_VALUE) {
+        return ERROR_VALUE;
+    }
+
+    /* ps1とps2の文字の大小比較を行う */
     for(int i = 0; i < MAX_STRING_VALUE; i++) {
         /* s1とs2に文字がまだあるかどうか確認 */
         if(*(ps1 + i) == '\0' && *(ps2 + i) == '\0') {
@@ -17,7 +29,7 @@ int scmp(char *ps1, char *ps2) {
         }else if(*(ps2 + i) == '\0'){
             return LARGER_PS1;
         }else{
-            continue;
+            /* 処理なし */
         }
 
         /* 文字の大小比較 */
@@ -26,11 +38,11 @@ int scmp(char *ps1, char *ps2) {
         }else if(*(ps1 + i) > *(ps2 + i)){
             return LARGER_PS1;
         }else{
-            continue;
+            /* 処理なし */
         }
     }
 
-    /* for()内で正常にreturnしなかった場合*/
+    /* for()内で正常にreturnしなかった場合 */
     return ERROR_VALUE;
 }
 
