afe0b7422311a78ee6106eb29dbe4109ee0dd2fd
yusuke.funabashi <kobarutogaeru@gmail.com>
2024-06-11 09:05:03 +0900

    Modify basic_C_03_01.c

--- 実践プログラミングC言語/basic_C_03_01.c
+++ 実践プログラミングC言語/basic_C_03_01.c
@@ -8,18 +8,28 @@ int main() {
     char *endptr;
     errno = 0;
 
-    /* 文字列"11"を10進数の数値(11)に変換 */
-    long decimal_value = strtol(decimal_string, &endptr, 10);
+    /* 文字列"11"を8進数の数値に変換 */
+    long oct_value = strtol(decimal_string, &endptr, 8);
     if (errno != 0) {
         printf("Conversion Error occured: errno = %d.\n", errno);
         return 0;
-    } else if (*endptr != '\0' || decimal_value == LONG_MAX || decimal_value == LONG_MIN) {
+    } else if (*endptr != '\0' || oct_value == LONG_MAX || oct_value == LONG_MIN) {
+        printf("Conversion Error occured: invalid input or overflow/underflow.\n");
+        return 0;
+    }
+
+    /* 文字列"11"を16進数の数値に変換 */
+    long hex_value = strtol(decimal_string, &endptr, 16);
+    if (errno != 0) {
+        printf("Conversion Error occured: errno = %d.\n", errno);
+        return 0;
+    } else if (*endptr != '\0' || hex_value == LONG_MAX || hex_value == LONG_MIN) {
         printf("Conversion Error occured: invalid input or overflow/underflow.\n");
         return 0;
     }
 
     /* 8進数として表示 */
-    printf("Octal: %lo\n", decimal_value);
+    printf("Octal: %ld\n", oct_value);
     /* 16進数として表示 */
-    printf("Hexadecimal: %lx", decimal_value);
+    printf("Hexadecimal: %ld", hex_value);
 }
