361a2b793ec03db22ed9caa72ad1339fc70af015
yusuke.funabashi <kobarutogaeru@gmail.com>
2024-06-11 10:27:23 +0900

    Update basic_C_05_01.c

--- 実践プログラミングC言語/basic_C_05_01.c
+++ 実践プログラミングC言語/basic_C_05_01.c
@@ -4,12 +4,13 @@ int main() {
     int yy, mm, dd;
     char name[20];
 
+    /* 入力を受け取る(%02dで2文字区切りにする) */
     printf(">>input(yymmdd name)? ");
-    scanf("%d %d %d %s", &yy, &mm, &dd, name);
+    scanf("%02d %02d %02d %s", &yy, &mm, &dd, name);
 
     /* 値を表示 */
-    printf("yy --> %d\n", yy);
-    printf("mm --> %d\n", mm);
-    printf("dd --> %d\n", dd);
+    printf("yy --> %02d\n", yy);
+    printf("mm --> %02d\n", mm);
+    printf("dd --> %02d\n", dd);
     printf("name --> %s\n", name);
 }
