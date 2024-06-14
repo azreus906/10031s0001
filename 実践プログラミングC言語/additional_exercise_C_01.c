b75b0e4d02bbb0575f94397ec9d4e56781c29604
yusuke.funabashi <kobarutogaeru@gmail.com>
2024-06-14 12:02:13 +0900

    Modigy to add lange check for x and N

--- 実践プログラミングC言語/additional_exercise_C_01.c
+++ 実践プログラミングC言語/additional_exercise_C_01.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
+#define MIN_N_VALUE 0
+#define MAX_N_VALUE 50
+#define MIN_X_VALUE 0
+#define MAX_X_VALUE 50
+
 double maclaurin_sin(double x, int n); /* マクローリン展開を使用してsin(x)を計算する関数 */
 double factorial(int n); /* 階乗を計算する関数 */
 
@@ -13,15 +18,24 @@ int main() {
     scanf("%lf", &x);
     printf("input N > ");
     scanf("%d", &N);
+
+    /* xとNの値のチェック */
+    if (x < MIN_X_VALUE || x > MAX_X_VALUE) {
+        printf("Error: xの値が範囲外です\n");
+        return 0;
+    } else if (N < MIN_N_VALUE || N > MAX_N_VALUE) {
+        printf("Error: Nの値が範囲外です\n");
+        return 0;
+    }
     
     /* 各項数についての近似値と比較を表示 */
-    printf("項数n: \t\t sin(x) \t\t\t マクローリン近似値 \t\t sin(x) - 近似値\n");
+    printf("項数n: \t\t sin(x) \t\t マクローリン近似値 \t\t sin(x) - 近似値 \n");
     for (int n = 1; n <= N; n++) {
         double sin_val = sin(x);
         double approx_val = maclaurin_sin(x, n);
         double difference = sin_val - approx_val;
         
-        printf("%d: \t\t %.15f \t\t %.15f \t\t %.15f \n", n, sin_val, approx_val, difference);
+        printf("%d: \t\t %-10.5f \t\t %-10.5f \t\t %-10.5f \n", n, sin_val, approx_val, difference);
     }
     
     return 0;
